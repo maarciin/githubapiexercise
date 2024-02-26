febab5747b2d55370b163370181bb508761cb5e6
MarcinG <gawlikmr@gmail.com>
2024-02-26 08:28:37 +0100

    Initial commit

---
+++ src/main/java/com/example/githubapiexercise/GithubapiexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubapiexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubapiexerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubapiexerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubapiexercise/GithubapiexerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubapiexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubapiexerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
